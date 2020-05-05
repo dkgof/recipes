93659dcc6c1019f5f1ec8ae2873ca92fb37ab53b
dkgof <rolf@fambagge.dk>
2020-05-05 19:13:32 +0200

    Fixed fileupload handler, uses listener now instead of fileUploadListener

--- src/main/java/dk/fambagge/recipes/web/servlets/JsonServlet.java
+++ src/main/java/dk/fambagge/recipes/web/servlets/JsonServlet.java
@@ -23,53 +23,70 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/json")
 public class JsonServlet extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
-    
+
     public JsonServlet() {
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setHeaders(resp);
+
         Set<Recipe> recipes = Recipe.getAll();
-        
+
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode recipesJsonArray = mapper.createArrayNode();
-        
+
         recipes.forEach((recipe) -> {
             ObjectNode recipesJson = recipesJsonArray.addObject();
-            
+
             recipesJson.put("id", recipe.getId());
             recipesJson.put("name", recipe.getName());
             recipesJson.put("servings", recipe.getServings());
             recipesJson.put("calories", recipe.getEnergyInCalories());
             try {
                 recipesJson.put("media", recipe.getPrimaryMedia().getThumbnailUrl(256));
-            } catch(Exception e) {
-                
+            } catch (Exception e) {
+
             }
-            
+
             ArrayNode ingredientsJsonArray = recipesJson.putArray("ingredients");
-            
-            recipe.getIngredients().forEach((ingredient)->{
+
+            recipe.getIngredients().forEach((ingredient) -> {
                 ObjectNode ingredientJson = ingredientsJsonArray.addObject();
                 ingredientJson.put("name", ingredient.getIngredient().getName());
                 ingredientJson.put("amount", ingredient.getAmount());
                 ingredientJson.put("unit", ingredient.getMeasure().getSymbol());
             });
-            
+
             ArrayNode stepsJsonArray = recipesJson.putArray("steps");
-            
-            recipe.getSteps().stream().sorted((rs1, rs2)->{
+
+            recipe.getSteps().stream().sorted((rs1, rs2) -> {
                 return Integer.compare(rs1.getSortOrder(), rs2.getSortOrder());
-            }).forEachOrdered((step)->{
+            }).forEachOrdered((step) -> {
                 ObjectNode stepJson = stepsJsonArray.addObject();
                 stepJson.put("description", step.getDescription());
             });
         });
-        
+
         String jsonString = recipesJsonArray.toPrettyString();
-        
+
         resp.getWriter().println(jsonString);
         resp.getWriter().flush();
+    }
+
+    //for Preflight
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        setHeaders(resp);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void setHeaders(HttpServletResponse resp) {
+        resp.setHeader("Content-Type", "application/json; charset=utf-8");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET");
     }
 }
