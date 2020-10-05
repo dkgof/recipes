f04254584d314e16a2ab8ad752863a178c500b50
dkgof <rolf@fambagge.dk>
2020-10-05 10:11:52 +0200

    Updated lots of libs.

--- src/main/java/dk/fambagge/recipes/Main.java
+++ src/main/java/dk/fambagge/recipes/Main.java
@@ -44,7 +44,7 @@ public class Main {
             webContext.setContextPath("/");
 
             List<String> webjars = new LinkedList<>();
-            webjars.add("omnifaces-3.7.1.jar");
+            webjars.add("omnifaces-3.8.jar");
             
             webContext.setConfigurations(new Configuration[]{
                 new AnnotationConfiguration(), new WebXmlConfiguration(),
